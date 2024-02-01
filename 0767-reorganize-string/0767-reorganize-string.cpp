134dffee13e9e606e854245ae049e965510e3f85
shreyas sot <136191807+SHREOT@users.noreply.github.com>
2024-02-01 13:12:19 +0530

    Time: 2 ms (56.99%), Space: 8.5 MB (9.45%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,70 @@
+class node{
+  
+    public:
+    char data;
+    int count;
+    node(char d,int c){
+        data =d;
+        count =c;
+    }
+};
+class compare{
+    public:
+    bool operator()(node a,node b){
+        return a.count<b.count;
+    }
+};
+
+
+class Solution {
+public:
+    string reorganizeString(string s) {
+    int freq[26]={0};
+        
+        for(int i=0;i<s.length();i++){
+            char ch = s[i];
+            freq[ch-'a']++;
+        }
+        
+        
+        priority_queue<node,vector<node>,compare>maxheap;
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                node temp(i+'a',freq[i]);
+                maxheap.push(temp);
+            }
+        }
+            string ans = "";
+        while(maxheap.size()>1){
+            node first = maxheap.top();
+            maxheap.pop();
+            node second =maxheap.top();
+            maxheap.pop();
+            ans+=first.data;
+            ans+=second.data;
+            first.count--;
+            second.count--;
+            if(first.count!=0){
+                maxheap.push(first);
+            }
+            if(second.count!=0){
+                maxheap.push(second);
+            }
+            
+        }
+        while(maxheap.size()==1){
+            node temp=maxheap.top();
+            maxheap.pop();
+            
+            if(temp.count==1){
+                ans+=temp.data;
+            }
+            else{
+                return "";
+            }
+            
+        }
+        return ans;
+    }
+    
+};
